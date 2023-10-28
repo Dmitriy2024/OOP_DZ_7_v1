3be8d55699b512ac0e869e961217c5d772d7a242
Dmitriy2024 <132061065+Dmitriy2024@users.noreply.github.com>
2023-10-28 11:44:44 +0300

    Update Master.java

--- lesson7/src/main/java/ru/geekbrains/lesson7/observer/Master.java
+++ lesson7/src/main/java/ru/geekbrains/lesson7/observer/Master.java
@@ -11,7 +11,7 @@ public class Master implements Observer{
 
     @Override
     public void receiveOffer(String nameCompany, int salary, int workingHours) {
-        if (this.salary <= salary || workingHours == 1){
+        if (this.salary <= salary && workingHours == 1){
             System.out.printf("Специалист %s: Мне нужна эта работа! (компания: %s; заработная плата: %d; режим работы (1-день, 0-ночь) %s)\n",
                     name, nameCompany, salary, workingHours);
             this.salary = salary;
